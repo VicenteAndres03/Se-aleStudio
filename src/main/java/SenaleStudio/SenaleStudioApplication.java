814c5b39230f94ed97ba457c336ccb638134fdb1
VicenteAndres03 <vicepachecoa@gmail.com>
2025-09-12 19:27:11 -0300

    Primer index del proyecto

---
+++ src/main/java/SenaleStudio/SenaleStudioApplication.java
@@ -0,0 +1,13 @@
+package SenaleStudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenaleStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenaleStudioApplication.class, args);
+	}
+
+}
