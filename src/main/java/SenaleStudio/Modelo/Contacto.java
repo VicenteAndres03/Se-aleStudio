685339462d27622d7ef1cffbf60bfd50212807cc
Vicente Pacheco <vicepachecoa@gmail.com>
2025-09-14 14:36:41 -0300

    aplicacion con postgresql

--- src/main/java/SenaleStudio/Modelo/Contacto.java
+++ src/main/java/SenaleStudio/Modelo/Contacto.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 
 @Entity
+@Table(name = "contacto")
 public class Contacto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
